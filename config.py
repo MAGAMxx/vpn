e53368198f618461953a9e9c263653bb9e08bae2
MAGAMxx <mohammadakubov@gmail.com>
2026-01-11 06:59:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,28 +3,23 @@
 BOT_TOKEN = "8570392401:AAFfowtqYzjxz-PCC-0IVJPx1xl5V03LCXk"
 ADMIN_ID = 8479289622
 
-# VPN сервер
+# Xray сервер
 SERVER_IP = "31.130.131.214"
 SERVER_PORT = 443
-SNI = "www.microsoft.com"
 PBK = "Fc5uuBATtxZ5wNmz0YxFfHZEN2H25Cqudse7PPW_wR0"
 FP = "chrome"
-
-# Панель 3x-ui / Xray
-XRAY_API_URL = "https://31.130.131.214:55694/xqAY0T10JV0Nut7YIp/xray-api"  # API панели
-XRAY_API_TOKEN = "ВАШ_API_KEY"  # токен панели (см. в настройках панели)
+SNI = "www.microsoft.com"
+XRAY_API_URL = "http://127.0.0.1:62789"
+XRAY_API_TOKEN = ""  # оставляем пустым, если нет токена панели
 
 # Оплата
 PAY_PHONE = "79283376737"
-PAY_BANK = "Ozon"
+PAY_BANK = "Озон"
 
 # Тарифы
 PRICES = {
     "1m": {"name": "1 месяц", "price": 149, "days": 30},
     "3m": {"name": "3 месяца", "price": 400, "days": 90},
     "6m": {"name": "6 месяцев", "price": 650, "days": 180},
-    "12m": {"name": "12 месяцев", "price": 1000, "days": 365},
+    "12m": {"name": "12 месяцев", "price": 1000, "days": 365}
 }
-
-# Реферальные бонусы
-REF_BONUS_DAYS = 10
