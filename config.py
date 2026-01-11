6709f0d40869e74a290e60cf5ff897a901ca2605
MAGAMxx <mohammadakubov@gmail.com>
2026-01-11 06:48:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # config.py
+
 BOT_TOKEN = "8570392401:AAFfowtqYzjxz-PCC-0IVJPx1xl5V03LCXk"
 ADMIN_ID = 8479289622
 
@@ -10,8 +11,8 @@ PBK = "Fc5uuBATtxZ5wNmz0YxFfHZEN2H25Cqudse7PPW_wR0"
 FP = "chrome"
 
 # Панель 3x-ui / Xray
-XRAY_API_URL = "https://31.130.131.214:55694/xqAY0T10JV0Nut7YIp/xray-api"  # URL панели
-XRAY_API_TOKEN = "ВАШ_API_KEY"          # Токен панели
+XRAY_API_URL = "https://31.130.131.214:55694/xqAY0T10JV0Nut7YIp/xray-api"  # API панели
+XRAY_API_TOKEN = "ВАШ_API_KEY"  # токен панели (см. в настройках панели)
 
 # Оплата
 PAY_PHONE = "79283376737"
@@ -24,3 +25,6 @@ PRICES = {
     "6m": {"name": "6 месяцев", "price": 650, "days": 180},
     "12m": {"name": "12 месяцев", "price": 1000, "days": 365},
 }
+
+# Реферальные бонусы
+REF_BONUS_DAYS = 10
