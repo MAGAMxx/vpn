7139334229c1f594444d24b9464d9b94a233c6e2
MAGAMxx <mohammadakubov@gmail.com>
2026-01-11 07:07:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,21 @@
-# config.py
-
-# --- Telegram ---
 BOT_TOKEN = "8570392401:AAFfowtqYzjxz-PCC-0IVJPx1xl5V03LCXk"
 ADMIN_ID = 8479289622
 
-# --- Оплата ---
-PAY_PHONE = "79283376737"
-PAY_BANK = "Озон"
-
-# --- VPN/Xray ---
 SERVER_IP = "31.130.131.214"
 SERVER_PORT = 443
 PBK = "Fc5uuBATtxZ5wNmz0YxFfHZEN2H25Cqudse7PPW_wR0"
 FP = "chrome"
-SNI = "www.microsoft.com,microsoft.com"
+SNI = "www.microsoft.com"
 
-# --- Xray API ---
-XRAY_API_URL = "http://127.0.0.1:62789"  # локальный API
-XRAY_API_TOKEN = ""  # оставьте пустым если токена нет
+XRAY_API_URL = "http://127.0.0.1:62789"  # Локальный API Xray
+XRAY_API_TOKEN = ""  # если требуется
 
-# --- Тарифы ---
+PAY_PHONE = "79283376737"
+PAY_BANK = "Озон"
+
 PRICES = {
     "1m": {"name": "1 месяц", "price": 149, "days": 30},
     "3m": {"name": "3 месяца", "price": 400, "days": 90},
     "6m": {"name": "6 месяцев", "price": 650, "days": 180},
-    "12m": {"name": "12 месяцев", "price": 1000, "days": 365},
+    "12m": {"name": "12 месяцев", "price": 1000, "days": 365}
 }
