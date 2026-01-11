13dd81460e6e6bdeb6a4a954a5552e91615aff0d
MAGAMxx <mohammadakubov@gmail.com>
2026-01-11 06:46:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
+# config.py
 BOT_TOKEN = "8570392401:AAFfowtqYzjxz-PCC-0IVJPx1xl5V03LCXk"
-ADMIN_ID = 8479289622  # твой Telegram ID
+ADMIN_ID = 8479289622
 
-VPN_CONFIG_PATH = "/etc/xray/config.json"  # путь к конфигу xray
-VPN_SERVICE_NAME = "xray"                  # для перезапуска systemctl
+# VPN сервер
+SERVER_IP = "31.130.131.214"
+SERVER_PORT = 443
+SNI = "www.microsoft.com"
+PBK = "Fc5uuBATtxZ5wNmz0YxFfHZEN2H25Cqudse7PPW_wR0"
+FP = "chrome"
 
+# Панель 3x-ui / Xray
+XRAY_API_URL = "https://31.130.131.214:55694/xqAY0T10JV0Nut7YIp/xray-api"  # URL панели
+XRAY_API_TOKEN = "ВАШ_API_KEY"          # Токен панели
+
+# Оплата
 PAY_PHONE = "79283376737"
 PAY_BANK = "Ozon"
 
+# Тарифы
 PRICES = {
     "1m": {"name": "1 месяц", "price": 149, "days": 30},
     "3m": {"name": "3 месяца", "price": 400, "days": 90},
