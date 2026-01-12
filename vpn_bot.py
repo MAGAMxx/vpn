cfdc096ed8c0dd17a5bde6f15eafe38e8331a631
MAGAMxx <mohammadakubov@gmail.com>
2026-01-12 12:06:00 +0300

    Update vpn_bot.py

--- vpn_bot.py
+++ vpn_bot.py
@@ -153,24 +153,16 @@ def generate_vless_link(u_uuid):
             f"&sni={SNI}&fp={FP}&pbk={PBK}&sid={SID}&spx=%2F# НИДЕРЛАНДЫ 🇳🇱 MAGAMIX")
 
 def generate_happ_deeplink(sub_id):
-    """Генерирует правильный deeplink для отображения MAGAMIX VPN в приложении"""
-    if not sub_id:
-        return None
+    if not sub_id: return None
     
-    # Ваш актуальный домен на Render
-    render_domain = "magamix-redirect.onrender.com"
+    domain = "magamix-redirect.onrender.com"
+    # Добавляем ?name= в конец ссылки подписки
+    encoded_name = "MAGAMIX%20VPN"
+    sub_url = f"https://{domain}/sub/{sub_id}?name={encoded_name}"
     
-    # Ссылка на подписку, которую будет опрашивать HAPP
-    # Мы ведем на /sub/, чтобы сработал заголовок с именем профиля
-    subscription_url = f"https://{render_domain}/sub/{sub_id}"
-    
-    # Формируем структуру happ://add/LINK
-    happ_protocol_link = f"happ://add/{subscription_url}"
-    
-    # Оборачиваем в ваш редиректор для открытия в 1 клик
-    final_deeplink = f"https://{render_domain}/url?url={happ_protocol_link}"
-    
-    return final_deeplink
+    happ_link = f"happ://add/{sub_url}"
+    # Финальная ссылка для кнопки "в 1 клик"
+    return f"https://{domain}/url?url={happ_link}"
 
 def get_remaining_time_str(end_date):
     end_date_aware = MOSCOW_TZ.localize(end_date)
