f481e1c20a03b57b871018c1afa9ce4256a25879
MAGAMxx <mohammadakubov@gmail.com>
2026-01-12 06:21:06 +0300

    Update vpn_bot.py

--- vpn_bot.py
+++ vpn_bot.py
@@ -69,7 +69,7 @@ def add_user_to_xray(user_uuid, email, days):
                 "expiryTime": expiry_time,
                 "enable": True,
                 "tgId": "",
-                "subId": sub_id  
+                "subId": sub_id,
                 "remark": "MAGAMIX |Нидерланды"
             }]
         })
